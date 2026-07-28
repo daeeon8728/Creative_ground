463e20b2d5089b3a322ba8e34540277132456584
daeeon8728 <daeeon8728@github.com>
2026-07-29 01:05:27 +0900

    cleanup

--- scratch.js
+++
@@ -1,8 +0,0 @@
-const manifoldModule = require('manifold-3d');
-console.log(manifoldModule);
-async function test() {
-  const Module = await manifoldModule();
-  Module.setup();
-  console.log("Keys in Module:", Object.keys(Module));
-}
-test().catch(console.error);
